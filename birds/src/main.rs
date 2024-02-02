7099dd09592f3a5eeb7b577ed17ab950e0204876
llwyd <tom@llwyd.io>
2024-02-02 20:34:51 +0000

    add reference for turning at edge of screen

--- birds/src/main.rs
+++ birds/src/main.rs
@@ -204,6 +204,12 @@ fn view(app: &App, model: &Model, frame: Frame){
     let win = app.window_rect();
     let draw = app.draw();
 
+     
+    draw.rect()
+        .x_y(0.0, 0.0)
+        .w_h(640.0 - 250.0, 480.0 - 250.0)
+        .color(DARKGREY);
+/*
     for bird in &model.bird{
         bird.draw_region(&draw);
     }
@@ -211,6 +217,7 @@ fn view(app: &App, model: &Model, frame: Frame){
     for bird in &model.bird{
         bird.draw_sep_region(&draw);
     }
+  */  
 
     for bird in &model.bird{
         bird.draw(&draw);
