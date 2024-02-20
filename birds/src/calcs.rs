cc472331e486d739aa3309bcf6b528471a9881c0
llwyd <tom@llwyd.io>
2024-02-20 22:35:47 +0000

    removed wrapping

--- birds/src/calcs.rs
+++ birds/src/calcs.rs
@@ -25,11 +25,13 @@ pub fn separation(bird: &mut Bird, other_birds: &Vec <Bird>)->f32{
 
     //let mut angle = (average.y - bird.position().y).atan2(average.x - bird.position().x);
     let mut angle = (bird.position().y - average.y).atan2(bird.position().x - average.x);
-    
+
+    /*
     if angle < 0.0{
         angle = angle + ( 2.0 * std::f32::consts::PI );
     }
-
+    */
+    
     println!("Separation:{:?} Angle:{}", average, rad_to_deg(angle));
 
     angle
